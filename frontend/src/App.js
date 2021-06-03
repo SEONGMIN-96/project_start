a15e4fa7b64a1190dc625872fa5299b1c77f3fd4
SEONGMIN-96 <sm961121@gmail.com>
2021-06-03 17:27:54 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+const App = () => {
+  return(<>
+  <nav>
+    <link to="...">Home</link>
+    <link to="....">Blog</link>
+    <link to="....">About Me</link>
+  </nav>
+
+  <h1>첫번째 크기 헤드라인</h1>
+  <h2>두번째 크기 헤드라인</h2>
+  <h3>세번째 크기 헤드라인</h3>
+  <h4>네번째 크기 헤드라인</h4>
+  <h5>다섯번쨰 크기 헤드라인</h5>
+  <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 ㅓㅅ이겠죠?</p>
+  <link to="http://www.google.com">가자 구글</link>
+  <hr/>
+ 
+  </>)
+}
+export default App
