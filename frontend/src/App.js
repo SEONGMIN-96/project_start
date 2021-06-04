fc34db9ba1e8dc7d1be7bdb038e6b5f5841dd0fa
SEONGMIN-96 <sm961121@gmail.com>
2021-06-04 18:04:11 +0900

    react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react'
+import SignUp from './User/SignUp'
+import Login from './User/Login'
+import UserList from './User/UserList'
 const App = () => {
   return(<>
-  <nav>
-    <link to="...">Home</link>
-    <link to="....">Blog</link>
-    <link to="....">About Me</link>
-  </nav>
-
-  <h1>첫번째 크기 헤드라인</h1>
-  <h2>두번째 크기 헤드라인</h2>
-  <h3>세번째 크기 헤드라인</h3>
-  <h4>네번째 크기 헤드라인</h4>
-  <h5>다섯번쨰 크기 헤드라인</h5>
-  <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 ㅓㅅ이겠죠?</p>
-  <link to="http://www.google.com">가자 구글</link>
-  <hr/>
- 
+  <SignUp/>
+  <Login/>
+  <UserList/>
   </>)
 }
 export default App
